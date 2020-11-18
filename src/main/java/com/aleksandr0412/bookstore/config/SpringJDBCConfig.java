320f2b3e2bf1df0937f60741d739cc5206a7e34f
Aleksandr <petrov.aleksandr0412@gmail.com>
2020-11-18 13:55:27 +0300

    add jdbc config

--- src/main/java/com/aleksandr0412/bookstore/config/SpringJDBCConfig.java
+++ src/main/java/com/aleksandr0412/bookstore/config/SpringJDBCConfig.java
@@ -4,38 +4,41 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.DependsOn;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.simple.SimpleJdbcCall;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 
 import javax.sql.DataSource;
 
 @Configuration
 public class SpringJDBCConfig {
 //
-//    @Bean
-//    @DependsOn("dataSource")
-//    public JdbcTemplate jdbcTemplate(DataSource dataSource) {
-//        var jdbcTemplate = new JdbcTemplate();
-//        jdbcTemplate.setDataSource(dataSource);
-//        return jdbcTemplate;
-//    }
-//
-//    @Bean
-//    @DependsOn("dataSource")
-//    public NamedParameterJdbcTemplate namedParameterJdbcTemplate(DataSource dataSource) {
-//        var namedParameterJdbcTemplate = new NamedParameterJdbcTemplate(dataSource);
-//        return namedParameterJdbcTemplate;
-//    }
-//
-//    @Bean
-//    @DependsOn("dataSource")
-//    public SimpleJdbcInsert simpleJdbcInsert(DataSource dataSource) {
-//        var simpleJdbcInsert = new SimpleJdbcInsert(dataSource);
-//        return simpleJdbcInsert;
-//    }
-//
-//    @Bean
-//    @DependsOn("dataSource")
-//    public SimpleJdbcCall simpleJdbcCall(DataSource dataSource) {
-//        var simpleJdbcCall = new SimpleJdbcCall(dataSource);
-//        return simpleJdbcCall;
-//    }
+    @Bean
+    @DependsOn("dataSource")
+    public JdbcTemplate jdbcTemplate(DataSource dataSource) {
+        JdbcTemplate jdbcTemplate = new JdbcTemplate();
+        jdbcTemplate.setDataSource(dataSource);
+        return jdbcTemplate;
+    }
+
+    @Bean
+    @DependsOn("dataSource")
+    public NamedParameterJdbcTemplate namedParameterJdbcTemplate(DataSource dataSource) {
+        NamedParameterJdbcTemplate namedParameterJdbcTemplate = new NamedParameterJdbcTemplate(dataSource);
+        return namedParameterJdbcTemplate;
+    }
+
+    @Bean
+    @DependsOn("dataSource")
+    public SimpleJdbcInsert simpleJdbcInsert(DataSource dataSource) {
+        SimpleJdbcInsert simpleJdbcInsert = new SimpleJdbcInsert(dataSource);
+        return simpleJdbcInsert;
+    }
+
+    @Bean
+    @DependsOn("dataSource")
+    public SimpleJdbcCall simpleJdbcCall(DataSource dataSource) {
+        SimpleJdbcCall simpleJdbcCall = new SimpleJdbcCall(dataSource);
+        return simpleJdbcCall;
+    }
 }
