dfa14b762da7658e60f96da960477a9ca371af5b
Aleksandr <petrov.aleksandr0412@gmail.com>
2020-11-18 18:02:58 +0300

    fix

--- src/main/java/com/aleksandr0412/bookstore/config/DataSourceConfig.java
+++ src/main/java/com/aleksandr0412/bookstore/config/DataSourceConfig.java
@@ -13,12 +13,10 @@ public class DataSourceConfig {
     @Bean
     public DataSource dataSource(Environment environment) {
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
-//        var dataSource = new SingleConnectionDataSource();
         dataSource.setDriverClassName(environment.getRequiredProperty("datasource.driver"));
         dataSource.setUrl(environment.getRequiredProperty("datasource.url"));
         dataSource.setUsername(environment.getRequiredProperty("datasource.username"));
         dataSource.setPassword(environment.getRequiredProperty("datasource.password"));
-//        dataSource.setSuppressClose(true);
         return dataSource;
     }
 }
