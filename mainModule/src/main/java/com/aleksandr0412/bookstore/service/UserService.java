b3ba5c9cd606d4147f1531bce98bacd940aba7a7
aleksandr0412 <petrov.aleksandr0412@gmail.com>
2020-12-01 12:39:18 +0300

    lesson 7+ - make multi project

---
+++ mainModule/src/main/java/com/aleksandr0412/bookstore/service/UserService.java
@@ -0,0 +1,34 @@
+package com.aleksandr0412.bookstore.service;
+
+import com.aleksandr0412.api.dto.UserDto;
+
+import java.util.UUID;
+
+/**
+ * UserService
+ */
+public interface UserService {
+    /**
+     * Создает нового пользователя
+     *
+     * @param userDto
+     * @return созданного пользователя
+     */
+    UserDto createUser(UserDto userDto);
+
+    /**
+     * Возвращает пользователя по первичному ключу
+     *
+     * @param id
+     * @return пользователя по пк
+     */
+    UserDto getUserByPK(UUID id);
+
+    /**
+     * Удаляет пользователя по первичному ключу
+     *
+     * @param id
+     * @return удаленного пользователя
+     */
+    UserDto deleteUserByPK(UUID id);
+}
